0b6e1f1b99328816436320368b1543fc1378d9b1
shreyas-11755 <sparrow.shreyas@gmail.com>
2022-07-10 14:13:26 +0530

    1. Refactor the whole code. 2. Resolve package import error

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='ai_automation',
-    version='1.0',
+    version='2.0',
     license='MIT',
     author="SheikhChili",
     author_email='sparrow.shreyas@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=['aiautomation', 'aiautomation/classification', 'aiautomation/mlpackage',
+              'aiautomation/multiclassification', 'aiautomation/regression', 'aiautomation/segmentation'],
+    # package_dir={'': 'src'},
     url='https://github.com/SheikhChili/aipackage',
-    keywords='ML Automation',
+    keywords='AI Automation',
     install_requires=[
         'scikit-learn', 'xgboost', 'catboost', 'lightgbm', 'numpy', 'pandas', 'tensorflow', 'imblearn', 'optuna',
         'tpot', 'hyperopt', 'yellowbrick', 'librosa', 'keras_tuner', 'spacy', 'unidecode', 'bs4', 'dtale',
